d36a222eef6e8cfd2ddc6979c9e60f6d8b06714c
Landon Carr <landoncarr@Landons-MacBook-Pro.local>
2018-03-22 14:20:04 -0600

    Finished Coder Swag

--- CoderSwag/CoderSwag/Controller/CategoriesVC.swift
+++ CoderSwag/CoderSwag/Controller/CategoriesVC.swift
@@ -6,51 +6,54 @@
 //  Copyright © 2018 Landon Carr. All rights reserved.
 //
 
+
 import UIKit
 
 class CategoriesVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    
-    
     @IBOutlet weak var categoryTable: UITableView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         categoryTable.dataSource = self
         categoryTable.delegate = self
-        
-        
-        
-        
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    
-       return DataService.instance.getCategory().count
-        
+        return DataService.instance.getCategory().count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) ->  UITableViewCell {
-        
-        
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell") as? CategoryCell {
-            
             let category = DataService.instance.getCategory()[indexPath.row]
-            
             cell.updateViews(category: category)
             return cell
-            
         } else {
-                return CategoryCell()
-         }
-            
+            return CategoryCell()
+        }
     }
-     
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategory()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductsVC {
+            let barBtn = UIBarButtonItem()
+            barBtn.title = ""
+            navigationItem.backBarButtonItem = barBtn
+            assert(sender as? Category != nil)
+            productsVC.initProducts(category: sender as! Category)
+        }
+    }
+    
 }
+
+
+
+
 
 
 
--- CoderSwag/CoderSwag/View/ProductCell.swift
+++ CoderSwag/CoderSwag/View/ProductCell.swift
@@ -15,4 +15,12 @@ class ProductCell: UICollectionViewCell {
     @IBOutlet weak var productPrice: UILabel!
     
     
+    func updateViews(product: Product) {
+        
+        productImage.image = UIImage(named: product.imageName)
+        productTitle.text = product.title
+        productPrice.text = product.price
+        
+    }
+    
 }
